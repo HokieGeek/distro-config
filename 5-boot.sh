c7b6c9f310542f622769e96e78587757e615280d
HokieGeek <andres.f.perez@gmail.com>
2013-11-13 11:41:59 -0500

    Added command-line argument to select either EFI or bios bootloader

--- 5-boot.sh
+++ 5-boot.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+boottype=$1
+
 ### EFI
-mount -t efivarfs efivarfs /sys/firmware/efi/efivars              # ignore if already mounted
-pacman -S grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
+[ "${boottype}" = "--efi" ] && {
+    mount -t efivarfs efivarfs /sys/firmware/efi/efivars              # ignore if already mounted
+    pacman -S grub efibootmgr
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
+}
 
 ### i386
-pacman -S grub-bios os-prober
-grub-install --target=i386-pc --recheck /dev/sda
-
+[ "${boottype}" = "--bios" ] && {
+    pacman -S grub-bios os-prober
+    grub-install --target=i386-pc --recheck /dev/sda
+}
 
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 grub-mkconfig -o /boot/grub/grub.cfg
