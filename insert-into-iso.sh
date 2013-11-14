f3d5201f91c7064a79bf02b7ed8d0f34107f6ddb
HokieGeek <andres.f.perez@gmail.com>
2013-11-13 19:16:32 -0500

    Was able to successfully create a new iso with what I need, so I think that's a win

--- insert-into-iso.sh
+++ insert-into-iso.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
-#TODO: any way to extract label out of an iso?
+here=$(cd `dirname $0`; pwd)
+iso=$1
+label=$(file ${iso} | awk -F"'" '{ print $2 }' | cut -d' ' -f1)
+name=$(basename $iso | sed 's/\(.*\)\.iso/\1/g')
 
-mkisofs -r -V ARCH_201310 -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o archlinux-2013.10.01-dual-config.iso archlinux-2013.10.01
+echo "Iso: '${iso}'"
+echo "Label: '${label}'"
+echo "Name: '${name}'"
+
+mkdir /tmp/${name}
+
+# Mount the iso to temp
+sudo mkdir /mnt/arch
+mount -o loop ${iso} /mnt/arch
+rsync -vr /mnt/arch/* /tmp/${name}
+umount /mnt/arch
+sudo rm -rf /mnt/arch
+
+# Copy distro-config to the extract directory
+rsync -vr --exclude=".git" ${here} /tmp/${name}
+
+# Now create the new iso!
+mkisofs -r -V ${label} -cache-inodes -J -l \
+    -b isolinux/isolinux.bin -c isolinux/boot.cat \
+    -no-emul-boot -boot-load-size 4 -boot-info-table \
+    -o ${name}-config.iso /tmp/${name}
+
+#mkisofs -r -V ARCH_201310 -cache-inodes -J -l \
+#    -b isolinux/isolinux.bin -c isolinux/boot.cat \
+#    -no-emul-boot -boot-load-size 4 -boot-info-table \
+#    -o archlinux-2013.10.01-dual-config.iso archlinux-2013.10.01
+
+rm -rf /tmp/${name}
