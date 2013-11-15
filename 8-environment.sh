b362bce55feeb15d310b054330fddf388ac71596
HokieGeek <andres.f.perez@gmail.com>
2013-11-15 05:40:26 -0500

    Made the ssh keys code a tad smarter

--- 8-environment.sh
+++ 8-environment.sh
@@ -20,20 +20,17 @@ git clone https://github.com/HokieGeek/dotfiles.git
 dotfiles/setup.sh
 
 echo "=====> Downloading and setting up my ssh keys"
-# TODO
-#tarball=wget -P ${HOME}/.ssh https://www.dropbox.com/s/24pg53g5onstqut/ssh-keys.tgz.gpg && \
+ssh_keys_tarball=wget -P ${HOME}/.ssh https://www.dropbox.com/s/24pg53g5onstqut/ssh-keys.tgz.gpg
+ssh_keys_name=`basename ${ssh_keys_tarball}`
 mkdir ${HOME}/.ssh
 $(cd $HOME/.ssh && \
-    wget -P ${HOME}/.ssh https://www.dropbox.com/s/24pg53g5onstqut/ssh-keys.tgz.gpg && \
-    gpg -d ssh-keys.tgz.gpg|tar -xvz)
+    wget -P ${HOME}/.ssh ${ssh_keys_tarball} && \
+    gpg -d ${ssh_keys_name} | tar -xvz && rm -rf ${ssh_keys_name})
 
 echo "=====> Enabling suspension on lid closing"
 sudo pacman -S acpid
 sed -e "s;logger 'LID closed';echo -n mem > /sys/power/state;" /etc/acpi/handler.sh > /tmp/handler.sh
 sudo mv /tmp/handler.sh /etc/acpi
-# sed -e 's;/usr/share/acpi-support/screenblank;echo -n mem > /sys/power/state;' /tmp/lid.sh > /etc/acpi/lid.sh
-# Replace: . /usr/share/acpi-support/screenblank
-#    with: echo -n mem > /sys/power/state
 
 echo "=====> Creating profile"
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' > ~/.zprofile
