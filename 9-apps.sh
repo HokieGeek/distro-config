a9762deb95ab11fdfdc7e1d6cf2f9a41f2a293cf
HokieGeek <andres.f.perez@gmail.com>
2013-11-14 22:24:07 -0500

    Forgot to unsudo the yaourt calls, as it's not necessary

--- 9-apps.sh
+++ 9-apps.sh
@@ -19,21 +19,21 @@ installAUR pa/package-query/package-query.tar.gz
 installAUR ya/yaourt/yaourt.tar.gz
 
 echo "=====> Installing chromium and plugins"
-sudo yaourt -S chromium
+yaourt -S chromium
 # Want to make sure that pipelight is installed *after* chromium
-sudo yaourt -S pipelight
+yaourt -S pipelight
 
 echo "=====> Installing some system tools"
 sudo pacman -S openssh rsync
 
 echo "=====> Installing programming tools"
-sudo yaourt -S eclipse eclipse-vrapper
+yaourt -S eclipse eclipse-vrapper
 # TODO: darcs
 #arduino
 sudo pacman -S mercurial scons minicom
 
 echo "=====> Installing dropbox"
-sudo yaourt -S dropbox dropbox-cli
+yaourt -S dropbox dropbox-cli
 
 echo "=====> Installing media tools"
 sudo pacman -S gimp vlc deluge
