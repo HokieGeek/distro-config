f90a549485f4be5592346944fb83657fb12607b7
HokieGeek <andres.f.perez@gmail.com>
2013-11-14 21:02:19 -0500

    Creating the .vim directory

--- 4-user.sh
+++ 4-user.sh
@@ -2,13 +2,15 @@
 
 myuser=$1
 
-pacman -S sudo bash-completion vim zsh
 echo "=====> Set root password: "
 passwd
+
+echo "=====> Creating user '$myuser': "
+pacman -S sudo bash-completion vim zsh
 useradd -m -g users -G wheel,storage,power,scanner,uucp -s /bin/zsh andres
-echo "=====> Set password for '$myuser': "
 passwd andres
 pacman -Ss sudo
+mkdir /home/andres/.vim
 
 echo "=====> Giving user sudo permissions"
 # TODO: can this be scripted? figure out this next line
