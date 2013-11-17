ba78a21715da3d498240c5b138d2b5f17c3174a9
HokieGeek <andres.f.perez@gmail.com>
2013-11-17 13:27:55 -0500

    Added gksudo

--- 9-apps.sh
+++ 9-apps.sh
@@ -53,5 +53,5 @@ echo "=====> Installing printer stuff"
 sudo pacman -S hplip cups cups-filters ghostscript gsfonts
 
 echo "=====> Installing various useful tools"
-sudo pacman -S virtualbox googlecl pkgfile x11vnc
+sudo pacman -S virtualbox googlecl pkgfile x11vnc gksudo
 sudo pkgfile --update
