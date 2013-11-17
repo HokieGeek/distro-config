7086ad5ebd550c1ce0927618465d8e701c139a56
HokieGeek <andres.f.perez@gmail.com>
2013-11-17 13:28:37 -0500

    Using vm from prop file instead of isVm

--- C-configure-user.sh
+++ C-configure-user.sh
@@ -10,7 +10,7 @@ ${mydir}/6-network.sh --${network}
 echo "======> Installing user applications"
 ${mydir}/7-xtools.sh
 ${mydir}/8-environment.sh
-[ ${isVm} -eq 1 ] && ${mydir}/vm.sh
+[ ${vm} -eq 1 ] && ${mydir}/vm.sh
 ${mydir}/9-apps.sh
 
 echo "Done. Starting X. Here goes nothing!"
