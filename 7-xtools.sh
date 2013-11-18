f92b8dec9d2aad5b060c2075f905e55abfd0f465
HokieGeek <andres.f.perez@gmail.com>
2013-11-17 19:53:02 -0500

    Added another package I think I need for the intel driver

--- 7-xtools.sh
+++ 7-xtools.sh
@@ -4,7 +4,7 @@ echo "=====> Installing Xorg Tools"
 sudo pacman -S xorg-server xorg-xinit xorg-utils xorg-server-utils xorg-twm xorg-xclock xterm
 
 echo "=====> Installing video driver"
-sudo pacman -S mesa xf86-video-intel lib32-mesa-libgl
+sudo pacman -S mesa xf86-video-intel lib32-intel-dri lib32-mesa-libgl
 
 echo "=====> Installing audio mixer and touchpad driver"
 sudo pacman -S alsa-utils xf86-input-synaptics
