c17780ca3c0d462baf0d57510abda2ac6ad18e5f
HokieGeek <andres.f.perez@gmail.com>
2013-11-17 20:49:19 -0500

    Removed commented out crap

--- B-root-setup.sh
+++ B-root-setup.sh
@@ -8,7 +8,6 @@ mydir=$(cd `dirname $0`; pwd)
 ${mydir}/3-packaging.sh
 ${mydir}/4-user.sh ${myuser}
 ## This box already has a bootloader in /boot (/dev/sda2)
-#[ ${bootloader} -gt 0 ] && ${mydir}/5-boot.sh --efi /dev/sda2
 [ ${bootloader} -gt 0 ] && ${mydir}/5-boot.sh --${bootloaderType}
 
 echo "exec ${mydir}/C-configure-user.sh" > /home/${myuser}/.zprofile
