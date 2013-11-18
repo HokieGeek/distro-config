585e8ff9d9258aa9dfca09c07dd483f540df0dd9
HokieGeek <andres.f.perez@gmail.com>
2013-11-17 19:53:50 -0500

    Thing didn't work at all. Now it does

--- vm.sh
+++ vm.sh
@@ -1,14 +1,13 @@
-pacman -S virtualbox-guest-utils
+sudo pacman -S virtualbox-guest-utils
 
-modprobe -a vboxguest vboxsf vboxvideo
+sudo modprobe -a vboxguest vboxsf vboxvideo
 
 cat << EOF > /etc/modules-load.d/virtualbox.conf
 vboxguest
 vboxsf
 vboxvideo
 EOF
+cp /tmp/virtualbox.conf /etc/modules-load.d
 
-Add the following line to the top of ~/.xinitrc above any exec options. (create a new file if it does not exist):
-cp {$HOME,/tmp}/.xinitrc
-sed -i '/bin\/sh/a\
-/usr/bin/VBoxClient-all' /tmp/.xinitrc > $HOME/.xinitrc
+#Add the following line to the top of ~/.xinitrc above any exec options. (create a new file if it does not exist):
+sed -i '/bin\/sh/a /usr/bin/VBoxClient-all' $HOME/.xinitrc
