ad4bb5cc0cc287c1114f492eec8d1ad2efe15e0a
HokieGeek <andres.f.perez@gmail.com>
2014-04-15 21:31:44 -0400

    Changing order of some of the user scripts

--- C-configure-user.sh
+++ C-configure-user.sh
@@ -9,9 +9,9 @@ ${mydir}/0-network.sh --${network}
 
 echo "======> Installing user applications"
 ${mydir}/6-xtools.sh ${myuser}
-${mydir}/7-environment.sh
+${mydir}/7-apps.sh
 [ ${vm} -eq 1 ] && ${mydir}/vm.sh
-${mydir}/8-apps.sh
+${mydir}/8-environment.sh
 
 echo "Done. Starting X. Here goes nothing!"
 exec startx
