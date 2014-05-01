c519289008ea4896d8f6a4b99c2dd10d0d2ec871
HokieGeek <andres.f.perez@gmail.com>
2014-04-30 20:15:45 -0400

    Refactored the network script

--- 0-network.sh
+++ 0-network.sh
@@ -1,58 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-interfaceType=$1
+if [ `pacman -Q1 netctl` -eq 1 -o `pacman -Q1 iw` -eq 1 ]; then
+    echo "=====> Installing networking tools"
+    pacman -S --needed netctl iw wpa_supplicant wpa_actiond dialog ifplugd
+fi
 
-[ "${interfaceType}" = "--eth" ] && {
-    [ "$#" -eq 1 ] && {
-        intf=`ip link | cut -d: -f2 | egrep -v "(00|lo)" | cut -d' ' -f2`
-        echo "=====> Setting up ethernet connection"
-        systemctl enable dhcpcd@${intf}.service
-        systemctl start dhcpcd@${intf}.service
-    }
-}
+wired=`iw dev | awk '$0 ~ /Interface/ { print $2 }' | egrep '^e'` # FIXME: UNTESTED!
+if [ ! -z "${wired}" ]; then
+    echo "=====> Setting up ethernet connection"
+    # TODO: cp /etc/netctl/examples/ethernet-dhcp /etc/netctl/default
+    # TODO: edit that file to be correct
+    systemctl enable netctl-ifplugd@${wired}.service
+    systemctl start netctl-ifplugd@${wired}.service
+fi
 
-[ "${interfaceType}" = "--wifi" ] && {
-    [ "$2" = "--install" ] && {
-        pacman -S --needed iw wpa_supplicant wpa_actiond dialog ifplugd
-    } || {
-        echo "=====> Setting up wireless connection"
-        # intf=`iw dev | cut -d: -f2 | egrep -v "(00|lo)" | cut -d' ' -f2`
-        intf=`iw dev | awk '$0 ~ /Interface/ { print $2 }'`
-
-        # TODO: select SSID and enter PWD
-        iw dev ${intf} scan | grep SSID
-
-        # ip link set dev ${intf} up
-        # iw dev ${intf} connect <SSID> key 0:<PWD>
-        # dhcpd ${intf}
-
-# echo "ExecStart=/usr/bin/iw dev %i connect <SSID> key 0:<PWD>" >> $servicefile
-        cat << EOF >/etc/systemd/system/wifi@.service
-[Unit]
-Description=Wifi connectivity (%i)
-Wants=network.target
-Before=network.target
-BindsTo=sys-subsystem-net-devices-%i.device
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-
-ExecStart=/usr/bin/ip link set dev %i up
-ExecStart=/usr/bin/wifi_connect
-ExecStart=/usr/bin/dhcpcd %i
-
-ExecStop=/usr/bin/ip link set dev %i down
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-        systemctl enable wifi@${intf}.service
-        systemctl start wifi@${intf}.service
-        iw dev ${intf} link
-    }
-}
-
+wireless=`iw dev | awk '$0 ~ /Interface/ { print $2 }' | egrep '^w'` # FIXME: UNTESTED!
+if [ ! -z "${wireless}" ]; then
+    echo "=====> Setting up wireless connection"
+    ip link set dev ${wireless} up
+    wifi-menu -o
+    systemctl enable netctl-auto@${wireless}.service
+    systemctl start netctl-auto@${wireless}.service
+fi
 ping -c 3 www.google.com
-# TODO: exit if this ping fails
+exit $?
