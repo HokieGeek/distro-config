abe49aaa9b10f3aadf4d4a59efc174c37f88916a
HokieGeek <andres.f.perez@gmail.com>
2014-05-03 09:40:53 -0400

    Finished the wired profile builder

--- 0-network.sh
+++ 0-network.sh
@@ -5,16 +5,17 @@ if [ `pacman -Q1 netctl` -eq 1 -o `pacman -Q1 iw` -eq 1 ]; then
     pacman -S --needed netctl iw wpa_supplicant wpa_actiond dialog ifplugd
 fi
 
-wired=`iw dev | awk '$0 ~ /Interface/ { print $2 }' | egrep '^e'` # FIXME: UNTESTED!
+wired=`iw dev | awk '$0 ~ /Interface/ { print $2 }' | egrep '^e'`
 if [ ! -z "${wired}" ]; then
     echo "=====> Setting up ethernet connection"
-    # TODO: cp /etc/netctl/examples/ethernet-dhcp /etc/netctl/default
-    # TODO: edit that file to be correct
+    f="/etc/netctl/default/${wired}"
+    cp /etc/netctl/examples/ethernet-dhcp ${f}
+    sed -i "s/\(Interface=\).*/\1${wired}/g" ${f}
     systemctl enable netctl-ifplugd@${wired}.service
     systemctl start netctl-ifplugd@${wired}.service
 fi
 
-wireless=`iw dev | awk '$0 ~ /Interface/ { print $2 }' | egrep '^w'` # FIXME: UNTESTED!
+wireless=`iw dev | awk '$0 ~ /Interface/ { print $2 }' | egrep '^w'`
 if [ ! -z "${wireless}" ]; then
     echo "=====> Setting up wireless connection"
     ip link set dev ${wireless} up
