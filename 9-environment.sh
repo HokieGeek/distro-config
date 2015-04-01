d7003e5fa16569e37699e7d897ee4a7194376146
HokieGeek <andres.f.perez@gmail.com>
2015-03-31 22:44:07 -0400

    Added some really good config tweaks to the system

--- 9-environment.sh
+++ 9-environment.sh
@@ -41,7 +41,7 @@ sudo systemctl start cronie.service
 echo "=====> Confuring fan controls"
 yaourt -S thinkfan lm_sensors
 
-cat << EOF > /tmp/thinkfan.conf
+sudo tee /etc/thinkfan.conf > /dev/null << EOF
 sensor /sys/devices/virtual/thermal/thermal_zone0/temp
 
 (0, 0, 40)
@@ -52,24 +52,41 @@ sensor /sys/devices/virtual/thermal/thermal_zone0/temp
 (5, 55, 67)
 (7, 61, 32767)
 EOF
-sudo mv /tmp/thinkfan.conf /etc
 sudo systemctl enable thinkfan
 
-echo "=====> Adjust the TrackPoint"
-cat << EOF > /tmp/10-trackpoint.rules
-SUBSYSTEM=="serio", DRIVERS=="psmouse", WAIT_FOR="speed", WAIT_FOR="sensitivity", \
-ATTR{sensitivity}="200", ATTR{speed}="180"
+echo "=====> Configure the TrackPoint"
+sudo tee /etc/udev/rules.d/10-trackpoint.rules > /dev/null << EOF
+SUBSYSTEM=="serio", DRIVER=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/press_to_select", \
+WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/speed", \
+ATTR{press_to_select}="1", ATTR{sensitivity}="230", ATTR{speed}="200"
 EOF
-sudo mv /tmp/10-trackpoint.rules /etc/udev/rules.d
+
+sudo tee /etc/X11/xorg.conf.d/20-thinkpad.conf > /dev/null << EOF
+Section "InputClass"
+    Identifier "Trackpoint Wheel Emulation"
+    MatchProduct    "TPPS/2 IBM TrackPoint|DualPoint Stick|Synaptics Inc. Composite TouchPad / TrackPoint|ThinkPad USB Keyboard with TrackPoint|USB TrackPoint pointing device"
+    MatchDevicePath "/dev/input/event*"
+    Option          "EmulateWheel"          "true"
+    Option          "EmulateWheelButton"    "2"
+    Option          "Emulate3Buttons"       "false"
+    Option          "XAxisMapping"          "6 7"
+    Option          "YAxisMapping"          "4 5"
+EndSection
+EOF
 
 echo "=====> Suspend when battery is low"
-cat << EOF > /tmp/99-lowbat.rules
+sudo tee /etc/udev/rules.d/99-lowbat.rules > /dev/null << EOF
 # Suspend the system when battery level drops to 2% or lower
 SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="2", RUN+="/usr/bin/systemctl suspend"
 SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="1", RUN+="/usr/bin/systemctl suspend"
 SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="0", RUN+="/usr/bin/systemctl suspend"
 EOF
-sudo mv /tmp/99-lowbat.rules /etc/udev/rules.d
+
+# echo "=====> Setting various power settings"
+## These settings were suggested when running powertop command
+# Enable Audio codec power management
+# sudo echo 1 > /sys/module/snd_hda_intel/parameters/power_save
+# sudo echo 0 > /proc/sys/kernel/nmi_watchdog
 
 #echo "=====> Installing SD Card driver"
 #http://www.realtek.com.tw/DOWNLOADS/RedirectFTPSite.aspx?SiteID=1&DownTypeID=3&DownID=951&PFid=25&Conn=3&FTPPath=ftp%3a%2f%2f208.70.202.219%2fpc%2fcrc%2frts_pstor.tar.bz2
