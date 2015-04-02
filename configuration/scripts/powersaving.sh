4e3fabdd873bc78a2c86e47fbbf126a9f8262abc
HokieGeek <andres.f.perez@gmail.com>
2015-04-02 10:09:29 -0400

    Putting in code for some more power saving

--- configuration/scripts/powersaving.sh
+++ configuration/scripts/powersaving.sh
@@ -2,7 +2,7 @@
 
 echo "=====> Setting various power saving settings"
 echo "====> Enable audio codec power management"
-echo "options snd_hda_intel powe_save=1" | sudo tee /etc/modprobe.d/audio_powersave.conf >/dev/null
+echo "options snd_hda_intel power_save=1" | sudo tee /etc/modprobe.d/audio_powersave.conf >/dev/null
 
 echo "====> Disable NMI watchdog"
 echo "kernel.nmi_watchdog = 0" | sudo tee /etc/sysctl.d/disable_watchdog.conf >/dev/null
@@ -13,7 +13,7 @@ sudo tee /etc/udev/rules.d/50-bluetooth.rules >/dev/null << EOF
 SUBSYSTEM=="rfkill", ATTR{type}=="bluetooth", ATTR{state}="0"
 EOF
 
-echo "====> Disable Wake-on LAN"
+echo "====> Disable Ethernet Wake-on LAN"
 sudo tee /etc/udev/rules.d/70-disable_wol.rules >/dev/null << EOF
 ACTION=="add", SUBSYSTEM=="net", KERNEL=="eth*", RUN+="/usr/bin/ethtool -s %k wol d"
 EOF
@@ -23,29 +23,20 @@ sudo tee /etc/udev/rules.d/70-wifi-powersave.rules >/dev/null << EOF
 ACTION=="add", SUBSYSTEM=="net", KERNEL=="wlan*", RUN+="/usr/bin/iw dev %k set power_save on"
 EOF
 
-# echo "====> VM writeback timeout"
+echo "====> Increasing virtual memory writeback timeout"
+echo "vm.dirty_writeback_centisecs = 1500" | sudo tee /etc/sysctl.d/dirty.conf >/dev/null
 
-# echo "====> Enabling SATA link power management"
+echo "====> Enabling USB autosuspend"
+sudo tee /etc/udev/rules.d/50-usb_power_save.rules >/dev/null << EOF
+# If USB devices are failing, go to https://wiki.archlinux.org/index.php/Power_saving#USB_autosuspend
+ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
+EOF
 
-# echo "====> Enabling autosuspend for some USB devices"
-
-# echo "====> Enabling power control for pci devices"
-# TODO: use lspci to determine which?
-# echo 'auto' > /sys/bus/pci/devices/0000:00:??.?/power/control
-#00:00.0 Host bridge: Intel Corporation 3rd Gen Core processor DRAM Controller (rev 09)
-#00:02.0 VGA compatible controller: Intel Corporation 3rd Gen Core processor Graphics Controller (rev 09)
-#00:14.0 USB controller: Intel Corporation 7 Series/C210 Series Chipset Family USB xHCI Host Controller (rev 04)
-#00:16.0 Communication controller: Intel Corporation 7 Series/C210 Series Chipset Family MEI Controller #1 (rev 04)
-#00:16.3 Serial controller: Intel Corporation 7 Series/C210 Series Chipset Family KT Controller (rev 04)
-#00:19.0 Ethernet controller: Intel Corporation 82579LM Gigabit Network Connection (rev 04)
-#00:1a.0 USB controller: Intel Corporation 7 Series/C210 Series Chipset Family USB Enhanced Host Controller #2 (rev 04)
-#00:1b.0 Audio device: Intel Corporation 7 Series/C210 Series Chipset Family High Definition Audio Controller (rev 04)
-#00:1c.0 PCI bridge: Intel Corporation 7 Series/C210 Series Chipset Family PCI Express Root Port 1 (rev c4)
-#00:1c.1 PCI bridge: Intel Corporation 7 Series/C210 Series Chipset Family PCI Express Root Port 2 (rev c4)
-#00:1c.2 PCI bridge: Intel Corporation 7 Series/C210 Series Chipset Family PCI Express Root Port 3 (rev c4)
-#00:1d.0 USB controller: Intel Corporation 7 Series/C210 Series Chipset Family USB Enhanced Host Controller #1 (rev 04)
-#00:1f.0 ISA bridge: Intel Corporation QM77 Express Chipset LPC Controller (rev 04)
-#00:1f.2 SATA controller: Intel Corporation 7 Series Chipset Family 6-port SATA Controller [AHCI mode] (rev 04)
-#00:1f.3 SMBus: Intel Corporation 7 Series/C210 Series Chipset Family SMBus Controller (rev 04)
-#02:00.0 Unassigned class [ff00]: Realtek Semiconductor Co., Ltd. RTS5229 PCI Express Card Reader (rev 01)
-#03:00.0 Network controller: Intel Corporation Centrino Advanced-N 6205 [Taylor Peak] (rev 34)
+echo "====> Enabling power control for pci devices"
+# TODO: is 50 ok?
+sudo tee /etc/udev/rules.d/50-pci_pm.rules >/dev/null <<EOF
+ACTION=="add", SUBSYSTEM=="pci", ATTR{power/control}="auto"
+EOF
+#for dev in `lspci | awk '$1 ~ /^00/ { print $1 }'`; do
+#    echo 'auto' | sudo tee /sys/bus/pci/devices/0000:${dev}/power/control >/dev/null
+#done
