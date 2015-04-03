6a44148b45bda9c6fee35682c4e019b1a6df1788
HokieGeek <andres.f.perez@gmail.com>
2015-04-03 04:37:50 -0400

    Updated boot script to just create a default config

--- boot.sh
+++ boot.sh
@@ -10,13 +10,13 @@ mydir=$(cd `dirname $0`; pwd)
 
     gummiboot install
 
+    ## Creatre a default config
     rootPartUUID=`blkid -s PARTUUID -o value ${partRoot}`
     {
         echo -e "title\t\tArch Linux"
         echo -e "linux\t\t/vmlinuz-linux"
         echo -e "initrd\t\t/initramfs-linux.img"
-        echo -e "options\t\troot=PARTUUID=${rootPartUUID} rw i915.i915_enable_rc6=1 i915.i915_enable_fbc=1 i915.lvds_downclock=1 i915.semaphores=1"
-        # echo -e "options\t\troot=PARTUUID=${rootPartUUID} rw"
+        echo -e "options\t\troot=PARTUUID=${rootPartUUID} rw"
 
     } > /boot/loader/entries/arch.conf
 }
