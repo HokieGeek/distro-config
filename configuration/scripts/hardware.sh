d6d222720fad0315770dc94ec7656748cd2b922d
HokieGeek <andres.f.perez@gmail.com>
2015-04-03 09:12:31 -0400

    Added SSD noatime optimization

--- configuration/scripts/hardware.sh
+++ configuration/scripts/hardware.sh
@@ -4,35 +4,18 @@ here=$(cd `dirname $0`; pwd)
 
 . ${here}/../config.prop
 
-# echo "=====> Change filesystem to noatime for SSDs"
-# TODO: needs to be smarter so that it only does this with / and /home
-# sudo sed -i 's/relatime/noatime/g' /etc/fstab
+# TODO: SDD-only {begin}
+echo "=====> Change filesystem to noatime for SSDs"
+awk '$2 ~ /^\/(home)?$/ { sub(/relatime/, "noatime") } { print }' /etc/fstab > /tmp/fstab && sudo mv /tmp/fstab /etc
 
-## 
-## /etc/fstab: static file system information
-##
-## <file system>	<dir>	<type>	<options>	<dump>	<pass>
-## /dev/sda2
-#UUID=4fd734f4-0bc9-4f0f-b696-0f5f398880d6	/         	ext4      	rw,noatime,data=ordered	0 1
-#
-## /dev/sda3
-#UUID=2df1cf7b-5dc4-45ed-8292-f8608a4454f7	/home     	ext4      	rw,noatime,data=ordered	0 2
-#
-## /dev/sda1
-#UUID=413A-A44D      	/boot     	vfat      	rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,errors=remount-ro	0 2
-#
-#/swapfile       	none      	swap      	defaults  	0 0
-
-# TODO: only do these next two if SSD set in config file
 echo "=====> Setting disk I/O scheduler for SSDs"
-# TODO: less dumb?
-# echo 'ACTION=="add|change", KERNEL=="'${device}', ATTR{queue/rotational}
 sudo tee /etc/udev/rules.d/60-schedulers.rules > /dev/null << EOF
 ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/schduler}="noop"
 EOF
 
 echo "=====> Setting swapiness to a low value"
 echo "vm.swappiness=10" | sudo tee /etc/sysctl.d/99-sysctl.conf >/dev/null
+# TODO: SSD-ONLY {end}
 
 echo "=====> Configuring bluetooth"
 sudo systemctl enable bluetooth
@@ -59,7 +42,7 @@ echo "=====> Configure the TrackPoint"
 sudo tee /etc/udev/rules.d/10-trackpoint.rules > /dev/null << EOF
 SUBSYSTEM=="serio", DRIVER=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/press_to_select", \
 WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/speed", \
-ATTR{press_to_select}="1", ATTR{sensitivity}="230", ATTR{speed}="200"
+ATTR{press_to_select}="1", ATTR{sensitivity}="200", ATTR{speed}="180"
 EOF
 
 sudo tee /etc/X11/xorg.conf.d/20-thinkpad.conf > /dev/null << EOF
