3340d54b1dc7fca09812c8641f5e70a0e6569263
HokieGeek <andres.f.perez@gmail.com>
2015-04-29 22:19:11 -0400

    Added ability to start the cvim server

--- configuration/scripts/xtools.sh
+++ configuration/scripts/xtools.sh
@@ -48,6 +48,7 @@ syndaemon -k -i 0.8 -d
 xrandr --output \`xrandr | awk '\$2 ~ /connected/{ print \$1 }'\` --auto
 xsetroot -cursor_name left_ptr
 ~/.bin/rotate-wallpaper ~/.look/bgs
+~/.bin/cvim-server start
 exec xmonad
 EOF
 
