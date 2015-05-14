71442b7e2115a022b279db2a608760702c9a5d39
HokieGeek <andres.f.perez@gmail.com>
2015-05-13 21:19:15 -0400

    I'll be honest. I have no idea if this'll work

--- configuration/scripts/kernel-params.sh
+++ configuration/scripts/kernel-params.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+## Add some values which I have now forgotten about but are related to performance
 sudo sed -i \
-    's/\s*options*/& i915.i915_enable_rc6=1 i915.i915_enable_fbc=1 i915.lvds_downclock=1 i915.semaphores=1' \
+    -e 's/\s*options*/& i915.i915_enable_rc6=1 i915.i915_enable_fbc=1 i915.lvds_downclock=1 i915.semaphores=1/' \
+    /boot/loader/entries/arch.conf
+
+sudo sed -i \
+    -e 's/\s*options*/& quiet loglevel=3 vga=current udev.log-priority=3/' \
     /boot/loader/entries/arch.conf
