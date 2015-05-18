44cfc47e83ce7226aefc8094004ac311b02f14fd
HokieGeek <andres.f.perez@gmail.com>
2015-05-17 21:15:19 -0400

    Added snotify to the list :)

--- configuration/scripts/x-config.sh
+++ configuration/scripts/x-config.sh
@@ -12,12 +12,12 @@ echo "=====> Creating xinitrc"
 cat << EOF > ~/.xinitrc
 #!/bin/sh
 
-#/usr/X11R6/bin/xautolock -time 10 -locker slock -secure -detectsleep
 syndaemon -k -i 0.8 -d
 xrandr --output \`xrandr | awk '\$2 ~ /connected/{ print \$1 }'\` --auto
 xsetroot -cursor_name left_ptr
 ~/.bin/rotate-wallpaper ~/.look/bgs
 ~/.bin/cvim-server start
+snotifyctl start
 exec xmonad
 EOF
 
