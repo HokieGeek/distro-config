dbf3881ce072bb16cbcf592c3dfd17625ab62e2b
HokieGeek <andres.f.perez@gmail.com>
2015-11-25 06:52:48 -0500

    Updating the AUR installation stuff based on the new AUR

--- installAUR.sh
+++ installAUR.sh
@@ -2,14 +2,15 @@
 
 pkg=`echo $1 | awk -F'/' '{ print $2 }'`
 
-pushd /tmp >/dev/null 2>&1
-wget https://aur.archlinux.org/packages/$1
-tar -xvzf `basename $1`
+pushd /tmp >/dev/null 3>&1
+# wget https://aur.archlinux.org/packages/$1
+git clone https://aur.archlinux.org/packages/$1
+# tar -xvzf `basename $1`
 
 pushd ${pkg} >/dev/null 2>&1
 
-makepkg -s
-sudo pacman -U --needed ${pkg}*.tar.xz
+makepkg -sri
+#sudo pacman -U --needed ${pkg}*.tar.xz
 
 popd >/dev/null 2>&1
 
