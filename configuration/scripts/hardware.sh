96d3f1b9d1ca1e54a64b9b7894e9392685a8aa0c
HokieGeek <andres.f.perez@gmail.com>
2015-11-29 21:27:42 -0500

    Adding rules for various devices

--- configuration/scripts/hardware.sh
+++ configuration/scripts/hardware.sh
@@ -61,6 +61,9 @@ Section "InputClass"
 EndSection
 EOF
 
+# sudo udevadm info --attribute-walk -n /dev/<DEV> | less
+# sudo udevadm control --reload-rules && sudo udevadm trigger
+
 echo "=====> Suspend when battery is low"
 sudo tee /etc/udev/rules.d/99-lowbat.rules >/dev/null << EOF
 # Suspend the system when battery level drops to 2% or lower
@@ -69,13 +72,26 @@ SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="1", RUN
 SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="0", RUN+="/usr/bin/systemctl suspend"
 EOF
 
-echo "=====> Add rule to specify nicer dev for the bus pirate"
+echo "=====> Add rules to specify nicer dev symlinks for some pluggable devices"
+echo "======> Bus Pirate"
 sudo tee /etc/udev/rules.d/98-buspirate.rules >/dev/null << EOF
-SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", GROUP="users", MODE="0666", SYMLINK+="buspirate"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="A900ftNY", GROUP="users", MODE="0666", SYMLINK+="buspirate"
 EOF
 
-echo "=====> Add rule to specify nicer dev for the Open Logic Sniffer"
+echo "======> Open Logic Sniffer"
 sudo tee /etc/udev/rules.d/98-openlogicsniffer.rules >/dev/null << EOF
 ATTRS{idVendor}=="04d8", ATTRS{idProduct}=="fc92", MODE="0666", SYMLINK+="ols"
 EOF
 
+echo "======> FTDI Basic 3.3V & 5V"
+sudo tee /etc/udev/rules.d/98-ftdibasic.rules >/dev/null << EOF
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AL01OVSC", GROUP="users", MODE="0666", SYMLINK+="ftdibasic3v3"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AH00S8CV", GROUP="users", MODE="0666", SYMLINK+="ftdibasic5v"
+EOF
+
+echo "======> Arduinos"
+sudo tee /etc/udev/rules.d/98-arduino.rules >/dev/null << EOF
+SUBSYSTEM=="tty", ATTRS{idProduct}=="6015", ATTRS{idVendor}=="0403", ATTRS{serial}=="ADAOJBOyA", GROUP="users", MODE="0666", SYMLINK+="expresscarduino"
+SUBSYSTEM=="tty", ATTRS{idProduct}=="6015", ATTRS{idVendor}=="0403", ATTRS{serial}=="ADAOKII3M", GROUP="users", MODE="0666", SYMLINK+="arduinoMetro328"
+SUBSYSTEM=="tty", ATTRS{idProduct}=="0043", ATTRS{idVendor}=="2341", ATTRS{serial}=="64935343533351116090", GROUP="users", MODE="0666", SYMLINK+="arduinoUno"
+EOF
